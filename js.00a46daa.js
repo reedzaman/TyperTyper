36f1ebffb67f694ebbbf296287ca3ce01eccf70e
Reed Zaman <reedzaman98@gmail.com>
2022-03-31 13:18:30 +0600

    Updates

--- js.00a46daa.js
+++ js.00a46daa.js
@@ -11845,7 +11845,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40411" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35205" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -12022,4 +12022,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/index.js"], null)
-//# sourceMappingURL=/js.00a46daa.js.map
+//# sourceMappingURL=js.00a46daa.js.map
